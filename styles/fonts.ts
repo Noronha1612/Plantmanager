cef1a958a46bca938cb40f788abd7840ae26cdf0
NoronhaGCP <inc.691@gmail.com>
2021-04-21 14:25:29 -0300

    add fonts to font object

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,5 +1,7 @@
 export default {
     heading: 'Jost_600SemiBold',
     text: 'Jost_400Regular',
-    complement: 'Jost_400Regular'
+    complement: 'Jost_400Regular',
+    thinText: 'Jost_300Light',
+    mediumText: 'Jost_500Medium'
 };
