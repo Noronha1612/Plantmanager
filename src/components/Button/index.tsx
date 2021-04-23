01fe352d2b65afede5291641e8c896f2a2e82ae5
NoronhaGCP <inc.691@gmail.com>
2021-04-23 09:48:32 -0300

    add loading animation to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { TouchableOpacityProps } from 'react-native';
+import React, { useState } from 'react';
+import { ActivityIndicator, TouchableOpacityProps } from 'react-native';
+import colors from '../../../styles/colors';
 
 import { ButtonContainer, ButtonText } from './styles';
 
 interface ButtonProps extends TouchableOpacityProps {
     text: string;
+    loading?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ text, style, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ text, style, loading, ...props }) => {
     return (
         <ButtonContainer {...props}>
-            <ButtonText>{ text }</ButtonText>
+            <ButtonText>{ 
+            loading ? 
+                <ActivityIndicator 
+                    color={ colors.white }
+                    size={ 24 }
+                /> : 
+                text 
+            }</ButtonText>
         </ButtonContainer>
     );
 }
